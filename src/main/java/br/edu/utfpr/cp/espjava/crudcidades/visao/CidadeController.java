31cef736ff7bc2fb43f6dbd40a5f45a0de5067e9
gmkoeb <gmkoeb@gmail.com>
2025-09-22 13:32:37 -0300

    feat: install freemarker and create dynamic landing page

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/visao/CidadeController.java
@@ -1,12 +1,22 @@
 package br.edu.utfpr.cp.espjava.crudcidades.visao;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
     @GetMapping("/")
-    public String listar(){
-        return "crud.html";
+    public String listar(Model memoria){
+        var cidades = Set.of(
+            new Cidade("Curitiba", "PR"),
+            new Cidade("Pomerode", "SC"),
+            new Cidade("Fortaleza", "CE"),
+            new Cidade("Cornélio Procópio", "PR")
+        );
+        memoria.addAttribute("listaCidades", cidades);
+        return "crud";
     }
 }
